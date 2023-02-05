f579319534c600b8b65e9e1a80c817627a461cf7
James Hooper <jameshooperdesigns@gmail.com>
2023-02-05 16:23:59 -0700

    Add more logic to javascript

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function sayHello() {
-  const name = prompt("Hi! What's your name?")
-  if (name !== null) {
-    window.alert(`It's nice to meet you ${name}! Thanks for checking out my website.`)
+function myFunction() {
+  var person = prompt("Hi! What's your name?");
+  if (person != null) {
+    document.getElementById("test").innerText = `Hi ${person}! Nice to meet you?`
   }
 }
