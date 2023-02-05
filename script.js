5d1aa60d38e8464106b7fe1abccff3066f1c82ea
James Hooper <jameshooperdesigns@gmail.com>
2023-02-05 16:40:16 -0700

    Fix the logic and save changes

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+const element = document.getElementById("quote-box");
+element.addEventListener("click", myFunction);
+
 function myFunction() {
   var person = prompt("Hi! What's your name?");
+  var quoteBox = document.querySelector('#quote-box')
+  quoteBox.classList.add('show');
   if (person != null) {
     document.getElementById("test").innerText = `Hi ${person}! Nice to meet you?`
   }
