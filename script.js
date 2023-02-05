166a6b08e5c1055b53cf41503552f1a33da8a114
James Hooper <jameshooperdesigns@gmail.com>
2023-02-05 13:14:42 -0700

    Add new logic with question in script

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function sayHello() {
   const name = prompt("Hi! What's your name?")
   if (name !== null) {
-    window.alert()
+    window.alert(`It's nice to meet you ${name}! Thanks for checking out my website.`)
   }
 }
